8f8fc7bd18aa6ef3554b91c3a837f3431a0a2529
Adedotun-Adepoju <d.e.adepoju@gmail.com>
2025-10-13 07:03:36 +0100

    initial commit

---
+++ src/test/java/com/example/ai_news_intelligence/AiNewsIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_news_intelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiNewsIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
